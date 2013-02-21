d21f0cf00cc5193b71b8728fea32dfae623ea2fe
linkkingjay <linkkingjay@gmail.com>
2013-02-21 16:43:52 +0800

    added templates/

---
+++ application/views/templates/header.php
@@ -0,0 +1,15 @@
+<DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Maureen's Message Board</title>
+    <link rel="stylesheet" href="css/main.css" />
+</head>
+<body>
+<div id="guide">
+<ul>
+    <li><a href="">Home</a></li>
+    <li><a href="">Say</a></li>
+    <li><a href="">About</a></li>
+</ul>
+</div>
