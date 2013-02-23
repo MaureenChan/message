fc9a3e0da2c61ee475b000df5c8d536c7bec4225
Maureen <Maureencqm@gmail.com>
2013-02-23 14:03:14 +0800

    updated message_model.php

--- application/models/message_model.php
+++ application/models/message_model.php
@@ -5,5 +5,34 @@ class Message extends CI_model {
         $this->load->database();
     }
 
+    public function get_message($id=FALSE)
+    {
+        if($id===FALSE)
+        {
+            $query=$this->db->get('message');
+            return $query->result_array();
+        }
+        $query=$this->db->get_where('message',array('id'=>$id));
+        return $query->row_array();
+    }
+
+
+    public function set_message()
+    {
+       
+        $data=array(
+            'user_name'=>$this->input->post('user_name'),
+            'content'=>$this->input->post('content'),
+            'date'=>date()
+        );
+        return $this->db->insert('message',$data);
+    }
+
+
+    public function delete_message($id=FALSE)
+    {
+        $this->db->delete('message',array('id'=>$id));
+
+    }
 }
 ?>
