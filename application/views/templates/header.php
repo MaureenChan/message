ef632fac87e8e57feba09edb6cee5cc70adf227b
linkkingjay <linkkingjay@gmail.com>
2013-02-23 15:14:17 +0800

    updated header.php

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -2,14 +2,14 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Maureen's Message Board</title>
+    <title><?php echo $title;?></title>
     <link rel="stylesheet" href="http://localhost/message/application/views/templates/css/main.css" />
 </head>
 <body>
 <div id="guide">
 <ul>
     <li><a href="">Home</a></li>
-    <li><a href="">Say</a></li>
+    <li><a href="">Add</a></li>
     <li><a href="">About</a></li>
 </ul>
 </div>
