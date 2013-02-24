2a0fd84bd7c6b33761b6a4982071cba8a04fb5f8
linkkingjay <linkkingjay@gmail.com>
2013-02-24 13:11:38 +0800

    removed some files

--- application/views/message/signin.php
+++
@@ -1,9 +0,0 @@
-<div>
-<?php echo form_open('message/user')?>
-<label for="username">username</label>
-<input type="input" name="username"/><br />
-<label for="password">password:</label>
-<input type="password" name="password"/><br />
-<input type="submit" name="submit"value="Sign in"/>
-</form>
-</div>
